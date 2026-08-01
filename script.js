28e7b131f283d0c9a3f44f78de7cac4452967eff
Ezequias <ezequiasoliver8@gmail.com>
2026-07-31 22:48:13 -0500

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-script.js
+const sunflower = document.getElementById("sunflower");
+const music = document.getElementById("bgMusic");
+
+sunflower.addEventListener("click", () => {
+
+    if (music.paused) {
+        music.play().catch(() => {});
+    }
+
+    sunflower.animate([
+        { transform: "scale(1)" },
+        { transform: "scale(1.18) rotate(8deg)" },
+        { transform: "scale(1)" }
+    ], {
+        duration: 500
+    });
+
+});
