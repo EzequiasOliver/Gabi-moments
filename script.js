11def3300fae82a0faf0f8bc57ec87c86c225ba6
Ezequias <ezequiasoliver8@gmail.com>
2026-08-04 21:03:27 -0500

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const sunflower = document.getElementById("sunflower");
 const music = document.getElementById("bgMusic");
 
+const memory = document.getElementById("memory");
+
+let opened = false;
+
 sunflower.addEventListener("click", () => {
 
+    // Inicia a música
     if (music.paused) {
         music.play().catch(() => {});
     }
 
+    // Animação do girassol
     sunflower.animate([
         { transform: "scale(1)" },
         { transform: "scale(1.18) rotate(8deg)" },
@@ -14,5 +20,16 @@ sunflower.addEventListener("click", () => {
     ], {
         duration: 500
     });
+
+    // Mostra a primeira lembrança
+    if (!opened) {
+
+        opened = true;
+
+        setTimeout(() => {
+            memory.classList.add("show");
+        }, 300);
+
+    }
 
 });
